f0f78f6b364111ac956c2dea02ac7865fc45e142
Clara Loforti <clara.loforti@gmail.com>
2025-12-10 18:04:56 +0100

    commenti

--- soluzione2/script.js
+++ soluzione2/script.js
@@ -7,7 +7,7 @@ bottone.addEventListener("click", () => {
     if (lampadina.src.includes('/img/white_lamp.png')) { // se src è white(spenta) falla diventare yellow(accesa) e scrivi "Spegni"
       lampadina.src = '/img/yellow_lamp.png';
       bottone.innerHTML = "Spegni";
-    } else {
+    } else { // altrimenti (quindi se non è white) falla diventare white(spenta) e scrivi "Accendi"
         lampadina.src = '/img/white_lamp.png';
         bottone.innerHTML = "Accendi";
     }
