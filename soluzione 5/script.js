d4feeb9da00a45e8c832cd64cccfeadd2f6dcb22
Clara Loforti <clara.loforti@gmail.com>
2025-12-10 17:59:17 +0100

    aggiungo soluzione 5

---
+++ soluzione 5/script.js
@@ -0,0 +1,17 @@
+// Quinta soluzione utilizzando includes (Miglioramento della terza soluzione)
+const lampadina = document.getElementById("immagine");
+const bottone = document.getElementById("tasto");
+let accesa = false;
+
+bottone.addEventListener("click", () => {
+
+    if (accesa) { // se è accesa, al click diventa bianca, il bottone cambia in "Accendi" e accesa diventa false
+        lampadina.src = '/img/white_lamp.png';
+        bottone.innerHTML = "Accendi";
+        accesa = false;
+    } else { // se è spenta, al click diventa gialla e il bottone cambia in "Spegni" e accesa diventa true
+        lampadina.src = '/img/yellow_lamp.png';
+        bottone.innerHTML = "Spegni";
+        accesa = true;
+    }
+});
