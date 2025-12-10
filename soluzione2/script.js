b724f1cd37c00d69b6088a58a645be1fb608b0ee
Clara Loforti <clara.loforti@gmail.com>
2025-12-10 17:33:02 +0100

    debug src imgs

--- soluzione2/script.js
+++ soluzione2/script.js
@@ -4,11 +4,11 @@ const bottone = document.getElementById("tasto");
 
 bottone.addEventListener("click", () => {
 
-    if (lampadina.src.includes('img/white_lamp.png')) { // se src è white(spenta) falla diventare yellow(accesa) e scrivi "Spegni"
-      lampadina.src = 'img/yellow_lamp.png';
+    if (lampadina.src.includes('/img/white_lamp.png')) { // se src è white(spenta) falla diventare yellow(accesa) e scrivi "Spegni"
+      lampadina.src = '/img/yellow_lamp.png';
       bottone.innerHTML = "Spegni";
     } else {
-        lampadina.src = 'img/white_lamp.png';
+        lampadina.src = '/img/white_lamp.png';
         bottone.innerHTML = "Accendi";
     }
 });
